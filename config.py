8a9ccead6960e22945e695af93abd10518a96f71
Rajat <rajatthakur6125@gmail.com>
2026-07-08 16:55:29 +0530

    Image_detection Model and other notebook

---
+++ config.py
@@ -0,0 +1,31 @@
+# ==========================
+# Dataset Configuration
+# ==========================
+
+DATASET_PATH = "data/raw/PlantVillage"
+
+# ==========================
+# Image Configuration
+# ==========================
+
+IMAGE_SIZE = 224
+
+# ==========================
+# Training Configuration
+# ==========================
+
+BATCH_SIZE = 32
+EPOCHS = 10
+LEARNING_RATE = 0.001
+
+# ==========================
+# Model Configuration
+# ==========================
+
+NUM_CLASSES = 15
+
+# ==========================
+# Save Paths
+# ==========================
+
+MODEL_PATH = "models/best_model.pth"
